0801cd729ead6fc32bd9d5b08a4dd4c12a3af5c3
Kahlib <kahlibstewart@gmail.com>
2022-12-10 22:14:09 -0500

    all done

--- Triangle Inside Angle Functionality Tests/Triangle Inside Angle Functionality Tests.cpp
+++ Triangle Inside Angle Functionality Tests/Triangle Inside Angle Functionality Tests.cpp
@@ -1,7 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
-extern "C" 
+extern "C" char* analyzeTriangle(int, int, int);
+extern "C" float* AngleFinder(int, int, int);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,8 +12,36 @@ namespace TriangleInsideAngleFunctionalityTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		//tests equilateral triangle
+		TEST_METHOD(AngleFinderTest1)
 		{
+			float* result = AngleFinder(10, 10, 10);
+			float* expected = {60, 60, 60};
+			Assert::AreEqual(result, expected);
+		}
+
+		//tests right angle triangle
+		TEST_METHOD(AngleFinderTest2)
+		{
+			float* result = AngleFinder(10, 10, 14.14);
+			float* expected = { 45, 45, 90 };
+			Assert::AreEqual(result, expected);
+		}
+
+		//testing whether 3 inputs form a triangle or not
+		TEST_METHOD(AngleFinderTest3)
+		{
+			char* result = analyzeTriangle(1, 10, 100);
+			char* expected = { "Not a triangle" };
+			Assert::AreEqual(result, expected);
+		}
+
+		//test scalene triangle
+		TEST_METHOD(AngleFinderTest4)
+		{
+			float* result = AngleFinder(10, 17.32, 20);
+			float* expected = { 60, 30, 90 };
+			Assert::AreEqual(result, expected);
 		}
 	};
 }
