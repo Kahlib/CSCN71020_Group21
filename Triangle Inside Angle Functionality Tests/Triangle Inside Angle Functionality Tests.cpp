87c44106778800121b62377714f68f13a942a22e
Kahlib <kahlibstewart@gmail.com>
2022-12-10 21:06:59 -0500

    First test case done

---
+++ Triangle Inside Angle Functionality Tests/Triangle Inside Angle Functionality Tests.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" 
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TriangleInsideAngleFunctionalityTests
+{
+	TEST_CLASS(TriangleInsideAngleFunctionalityTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -11,15 +11,37 @@ namespace UnitTest1
 	{
 	public:
 		
+		//testing whether 3 inputs form a scalene triangle or not
 		TEST_METHOD(TestMethod1)
 		{
-			char* status = analyzeTriangle(4, 5, 6);
-			char* expected = { "Scalene Triangle" };
-			Assert::AreEqual(status, expected);
+			char* result = analyzeTriangle(4, 5, 6);
+			char* expected = { "Scalene triangle" };
+			Assert::AreEqual(result, expected);
 		}
 
+
+		//testing whether 3 inputs form an isosceles triangle or not
 		TEST_METHOD(TestMethod2)
 		{
+			char* result = analyzeTriangle(4, 4, 1);
+			char* expected = { "Isosceles triangle" };
+			Assert::AreEqual(result, expected);
+		}
+
+		//testing whether 3 inputs form an equilateral triangle or not
+		TEST_METHOD(TestMethod3)
+		{
+			char* result = analyzeTriangle(10, 10, 10);
+			char* expected = { "Equilateral triangle" };
+			Assert::AreEqual(result, expected);
+		}
+
+		//testing whether 3 inputs form a triangle or not
+		TEST_METHOD(TestMethod4)
+		{
+			char* result = analyzeTriangle(1, 10, 100);
+			char* expected = { "Not a triangle" };
+			Assert::AreEqual(result, expected);
 		}
 	};
 }
